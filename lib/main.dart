fc470f37c543b36ca715311e482bd28fb16c0de1
Fakhri Rasyid Saputro <gomastoliq@gmail.com>
2023-12-29 20:44:46 +0700

    Add Search - Provider State Management

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:roasty2/screen/detail_page.dart';
+import 'package:roasty2/screen/home_page.dart';
 import 'package:roasty2/screen/home_screen.dart';
+import 'package:roasty2/screen/splash_screen.dart';
+import 'package:roasty2/theme/styles.dart';
+import 'package:roasty2/screen/search_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +16,62 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      themeMode: ThemeMode.system,
       title: 'Roasty',
+      darkTheme: ThemeData(
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+          primary: primaryColorDark,
+          onPrimary: Colors.white,
+          secondary: secondaryColorDark,
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: secondaryColorDark,
+            elevation: 5,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(0),
+              ),
+            ),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(elevation: 0),
+        textTheme: myTextTheme,
+        primarySwatch: Colors.deepPurple,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
       theme: ThemeData(
-          primarySwatch: Colors.red,
-          visualDensity: VisualDensity.adaptivePlatformDensity),
-      initialRoute: HomeScreen.routeName,
+        colorScheme: Theme.of(context).colorScheme.copyWith(
+              primary: primaryColor,
+              onPrimary: Colors.black,
+              secondary: secondaryColor,
+            ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: secondaryColor,
+            elevation: 5,
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(
+                Radius.circular(0),
+              ),
+            ),
+          ),
+        ),
+        appBarTheme: const AppBarTheme(elevation: 0),
+        textTheme: myTextTheme,
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      initialRoute: SplashScreen.routeName,
       routes: {
+        SplashScreen.routeName: (context) => const SplashScreen(),
+        HomePage.routeName: (context) => const HomePage(),
         HomeScreen.routeName: (context) => const HomeScreen(),
+        DetailScreen.routeName: (context) => DetailScreen(
+              restaurantId:
+                  ModalRoute.of(context)?.settings.arguments as String,
+            ),
+        SearchScreen.routeName: (context) => const SearchScreen(),
       },
     );
   }
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,51 +1,97 @@
-import 'dart:math';
-
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:roasty2/model/restaurant_response.dart';
+import 'package:provider/provider.dart';
+import 'package:roasty2/provider/restaurant_provider.dart';
+import 'package:roasty2/utils/result_state.dart';
+import 'package:roasty2/widget/card_restaurant.dart';
+import 'package:roasty2/widget/platform_widget.dart';
+import 'package:roasty2/screen/search_screen.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   static const routeName = '/home_screen';
 
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Roasty"),
-      ),
-      body: FutureBuilder<String>(
-          future: DefaultAssetBundle.of(context)
-              .loadString('assets/local_restaurant.json'),
-          builder: (context, snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              final restaurantResponse =
-              restaurantResponseFromJson(snapshot.data!);
-              final restaurants = restaurantResponse.restaurants;
-              print("yayaya ***${restaurants}***");
-              return ListView.builder(
-                itemCount: restaurants.length,
-                itemBuilder: (context, index) {
-                  return _buildRestaurantItem(context, restaurants[index]);
-                },
-              ); 
-            } else {
-              return Text('data');
-            }
-          }),
-    );
-  }
+  State<HomeScreen> createState() => _HomeScreen();
 }
 
-Widget _buildRestaurantItem(BuildContext context, Restaurant restaurant) {
-  return ListTile(
-    contentPadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
-    leading: Image.network(
-      restaurant.pictureId,
-      width: 100,
-      errorBuilder: (ctx, error, _) => const Center(child: Icon(Icons.error)),
-    ),
-    title: Text(restaurant.name),
-    subtitle: Text(restaurant.id),
-  );
+class _HomeScreen extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Widget _buildList(BuildContext context) {
+    return Consumer<RestaurantProvider>(
+      builder: (context, state, _) {
+        if (state.state == ResultState.loading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        } else if (state.state == ResultState.hasData) {
+          return ListView.builder(
+            shrinkWrap: true,
+            itemCount: state.result.restaurants.length,
+            itemBuilder: (context, index) {
+              var restaurant = state.result.restaurants[index];
+              return CardRestaurant(restaurant: restaurant);
+            },
+          );
+        } else if (state.state == ResultState.error) {
+          return Center(
+            child: Material(
+              child: Text(state.message),
+            ),
+          );
+        } else {
+          return const Center(
+            child: Material(
+              child: Text(""),
+            ),
+          );
+        }
+      },
+    );
+  }
+
+
+  Widget _buildAndroid(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Roasty'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            color: const Color(0xFFc80064),
+            onPressed: () {
+              Navigator.pushNamed(
+                context,
+                SearchScreen.routeName,
+              );
+            },
+          ),
+        ],
+      ),
+      body: _buildList(context),
+    );
+  }
+
+  Widget _buildIos(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('Roasty'),
+        transitionBetweenRoutes: false,
+      ),
+      child: _buildList(context),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PlatformWidget(
+      androidBuilder: _buildAndroid,
+      iosBuilder: _buildIos,
+    );
+  }
 }
